aff0e2a636a3238ec5077d17179a3a03f0dc2499
jsm174 <jsm174@gmail.com>
2021-09-13 13:28:16 -0400

    ci: update formula
    Closes #1.
    
    Signed-off-by: Jason Millard <1197137+jsm174@users.noreply.github.com>

--- Casks/vulkan-sdk.rb
+++ Casks/vulkan-sdk.rb
@@ -3,20 +3,26 @@ cask "vulkan-sdk" do
   sha256 "0e2f9bf489988211480e0530299096cdfa2650ee120337417cd8f439592abd68"
 
   url "https://sdk.lunarg.com/sdk/download/#{version}/mac/vulkansdk-macos-#{version}.dmg?Human=true"
+  name "VulkanSDK"
+  desc "LunarG Vulkan development tools"
+  homepage "https://www.lunarg.com/vulkan-sdk/"
 
   installer script: {
-      executable: "#{staged_path}/InstallVulkan.app/Contents/MacOS/InstallVulkan",
-      args:       ["--root", "#{staged_path}", "--accept-licenses", "--accept-messages", "--confirm-command", "install"],
-      sudo:       true,
+    executable: "#{staged_path}/InstallVulkan.app/Contents/MacOS/InstallVulkan",
+    args:       [
+      "--root", staged_path.to_s,
+      "--accept-licenses",
+      "--accept-messages",
+      "--confirm-command", "install"
+    ],
+    sudo:       true,
   }
 
   uninstall script: {
-      executable: "#{staged_path}/UninstallVulkan.app/Contents/MacOS/UninstallVulkan",
-      args:       ["--confirm-command", "purge"],
-      sudo:       true,
+    executable: "#{staged_path}/UninstallVulkan.app/Contents/MacOS/UninstallVulkan",
+    args:       [
+      "--confirm-command", "purge"
+    ],
+    sudo:       true,
   }
-
-  name "VulkanSDK"
-  desc "Collection of essential tools used by developers to assist in development and debugging of Vulkan applications."
-  homepage "https://www.lunarg.com/vulkan-sdk/"
 end
