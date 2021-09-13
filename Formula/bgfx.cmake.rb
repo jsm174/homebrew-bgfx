7cee0d29492d22c83f0c789deec1073931792b3a
jsm174 <jsm174@gmail.com>
2021-09-13 13:28:16 -0400

    ci: update formula

--- Formula/bgfx.cmake.rb
+++ Formula/bgfx.cmake.rb
@@ -1,20 +1,39 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class BgfxCmake < Formula
-  desc "Independently maintained CMake build scripts for bgfx."
+  desc "Independently maintained CMake build scripts for bgfx"
   homepage "https://github.com/bkaradzic/bgfx.cmake"
-  license "https://github.com/bkaradzic/bgfx.cmake/blob/master/LICENSE"
-  head "https://github.com/bkaradzic/bgfx.cmake.git"
+  url "https://github.com/bkaradzic/bgfx.cmake/archive/76e5f9985d96c3390598b7213ded7ef46eb49a16.tar.gz"
+  sha256 "825a88bac01cc4e18a8ffa16198189c23ea3638115efd25a07e5cd858c7a3503"
+  license "CC0-1.0"
+  head "https://github.com/bkaradzic/bgfx.cmake.git", branch: "master"
 
   depends_on "cmake" => :build
 
+  resource "bgfx" do
+    url "https://github.com/bkaradzic/bgfx.git", revision: "9ecd4625388f3ff4601bb05e37f7bb6414203a73"
+  end
+
+  resource "bimg" do
+    url "https://github.com/bkaradzic/bimg.git", revision: "6693de0e50ff7e76a22d6f37251fa2dec12168cd"
+  end
+
+  resource "bx" do
+    url "https://github.com/bkaradzic/bx.git", revision: "e50536ac03eb404f50123d23f2c9ab2b3e4663e7"
+  end
+
   def install
-    system "cmake", ".", "-B", "build"
-    system "make", "-C", "build", "install"
+    unless build.head?
+      (buildpath/"bgfx").install resource("bgfx")
+      (buildpath/"bimg").install resource("bimg")
+      (buildpath/"bx").install resource("bx")
+    end
+
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make", "install"
+    end
   end
 
   test do
-    system "false"
+    system "#{bin}/shaderc", "-v"
   end
 end
