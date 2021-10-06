a786f9950faee578207b320509847086165daa9a
jsm174 <jsm174@gmail.com>
2021-10-06 06:36:34 -0400

    update: updat bgfx to v1.115.7933-417f8b8
    Closes #6.
    
    Signed-off-by: Jason Millard <1197137+jsm174@users.noreply.github.com>

--- Formula/bgfx.rb
+++ Formula/bgfx.rb
@@ -1,44 +1,25 @@
 class Bgfx < Formula
   desc "Cross-platform, graphics API agnostic, rendering library"
   homepage "https://github.com/bkaradzic/bgfx"
-  url "https://github.com/bkaradzic/bgfx/archive/403b69db7876bc7269f546273bba57c40d2b137e.tar.gz"
-  version "1.115.7816"
-  sha256 "f6808e6011e93470d1c60be6d371e905e5c703067dc0ee21b7c8943c60f8e042"
-  license "BSD-2-Clause"
-  head "https://github.com/bkaradzic/bgfx.git", branch: "master"
-
-  bottle do
-    root_url "https://github.com/jsm174/homebrew-bgfx/releases/download/bgfx-1.115.7816"
-    sha256 cellar: :any_skip_relocation, big_sur:  "5db79d3481e7731a06587277606bd2dc54a0c240ea83aa050ab7360f0c393538"
-    sha256 cellar: :any_skip_relocation, catalina: "84ec8151cdf6d3767c95192cbb250e2c639b24c566dc15aef0b19bba1c5004de"
-  end
+  url "https://github.com/bkaradzic/bgfx.cmake.git",
+      tag:      "v1.115.7933-417f8b8",
+      revision: "417f8b82ef76ef19a0f3efd93d32088c9b3331a0"
+  license any_of: ["CC0-1.0", "BSD-2-Clause"]
+  head "https://github.com/bkaradzic/bgfx.cmake.git", branch: "master"
 
   depends_on "cmake" => :build
 
   on_linux do
+    depends_on "gcc"
     depends_on "mesa"
   end
 
-  resource "bimg" do
-    url "https://github.com/bkaradzic/bimg/archive/6693de0e50ff7e76a22d6f37251fa2dec12168cd.tar.gz"
-    sha256 "d5413c1518367549f52bd2d99ced06d91bc8d4a88469007697ea2b8af8141e97"
-  end
-
-  resource "bx" do
-    url "https://github.com/bkaradzic/bx/archive/e50536ac03eb404f50123d23f2c9ab2b3e4663e7.tar.gz"
-    sha256 "f182558d2f87197e86a5c428af4e2ccdbf6e264fbe2b66ff10d10a6013873422"
-  end
-
-  resource "bgfx.cmake" do
-    url "https://github.com/bkaradzic/bgfx.cmake/archive/2c37f3ec62ae1f93525143358ad550b50edfbc55.tar.gz"
-    sha256 "ffa83286f091a7bec5edf2ddd91ba1515a08864107622f8778bdd7a78d314619"
-  end
-
   def install
-    (buildpath/"bgfx").install Dir["*"]
-    (buildpath/"bimg").install resource("bimg")
-    (buildpath/"bx").install resource("bx")
-    buildpath.install resource("bgfx.cmake")
+    rev = Utils.safe_popen_read("git", "--git-dir", "bgfx/.git", "rev-list", "HEAD", "--count").strip
+    sha1 = Utils.safe_popen_read("git", "--git-dir", "bgfx/.git", "rev-parse", "HEAD").strip
+
+    inreplace "bgfx/src/version.h", /BGFX_REV_NUMBER.*$/, "BGFX_REV_NUMBER #{rev}"
+    inreplace "bgfx/src/version.h", /BGFX_REV_SHA1.*$/, "BGFX_REV_SHA1 \"#{sha1}\""
 
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
